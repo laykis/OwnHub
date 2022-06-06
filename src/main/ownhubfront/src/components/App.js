b4e69f304e4110392e489ceaa481098104bcd8de
laykis <flash6471@naver.com>
2022-06-06 18:18:40 +0900

    try to fix React.js

--- src/main/ownhubfront/src/components/App.js
+++ src/main/ownhubfront/src/components/App.js
@@ -1,27 +1,16 @@
-<<<<<<< HEAD
-
+import React from 'react';
 import Login from 'Login';
-import { Route, BrowserRouter as Router } from "react-router-dom";
-import {render} from "react-dom";
-
-=======
-import React, {useEffect, useState} from "react";
-import axios from "axios";
-import Login from 'Login';
->>>>>>> refs/remotes/origin/master
-
 
 
 function App() {
 
     return(
-        <Router>
-            <Route exact path="" component={Login} />
-        </Router>
+        <div>
+            <Login />
+        </div>
 
     );
 
 }
-
 
 export default App;
--- src/main/ownhubfront/src/index.js
+++ src/main/ownhubfront/src/index.js
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
 import React, {Component, Fragment} from 'react';
-import ReactDOM from 'react-dom';
-=======
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-
->>>>>>> refs/remotes/origin/master
-import App from 'components/App';
+import App from '@components/App';
 
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+root.render(<App />);
 
 
