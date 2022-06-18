fe05536170241683ce934eb1d1663c7a63c942d4
laykis <flash6471@naver.com>
2022-06-18 20:12:31 +0900

    Jgit test

--- src/main/java/Kim/OwnHub/controller/HelloController.java
+++ src/main/java/Kim/OwnHub/controller/HelloController.java
@@ -1,7 +1,12 @@
 package Kim.OwnHub.controller;
 
+import Kim.OwnHub.git.Jgit;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
 
 @RestController
 public class HelloController {
@@ -11,5 +16,19 @@ public class HelloController {
     public String helloworld(){
 
         return "Hello world";
+    }
+
+    private File dir = new File("/Users/laykis/Desktop/gitTest");
+
+    @GetMapping("")
+    public void init(){
+        try{
+            Jgit.init(dir);
+            Git.open(dir).checkout();
+            Repository repository = Git.cloneRepository().getRepository();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
