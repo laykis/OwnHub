eb95ccb1a51017defeea2716c4c03a4f6d66bbb8
Goinging98 <yunw98@naver.com>
2022-08-02 00:14:03 +0900

    [webfront] workday.js update

--- src/main/ownhubfront/src/function/Work_day.js
+++ src/main/ownhubfront/src/function/Work_day.js
@@ -1,27 +1,38 @@
 import React, {useState} from "react";
 // import axios from "axios";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import "../each_page.css";
 
 const Workday = () => {
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate,setEndDate] = useState(new Date());
+
     return (
         <div>
-            <div id="vacation">
-                <h2>휴가 신청</h2>
-                <div>
-                    <select id="cboard">
-                        <option value="choice">(선택)</option>
-                        <option value="full">연차</option>
-                        <option value="half">반차</option>
-                    </select>
+            <body id="body">
+                <div id="vacation">
+                    <h2>휴가 신청</h2>
+                    <div>
+                        <p>휴가일자</p>
+                        <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
+                        <DatePicker selected={endDate} onChange={date => setEndDate(date)}/>
+                    </div>
+                    <div>
+                        <p>휴가종류</p>
+                        <select id="cboard">
+                            <option value="choice">(선택)</option>
+                            <option value="full">연차</option>
+                            <option value="half">반차</option>
+                        </select>
+                    </div>
+                    <div>
+                        <p>사유</p>
+                        <input id = "reason" type="text" name="title" placeholder="사유를 작성해주세요" />
+                    </div>
+                    <button type="button" id="submitBtn">기안</button>
                 </div>
-                <div>
-                    <input id = "title" type="text" name="title" value={title} onChange={handleTitle} placeholder="제목을 입력하세요." />
-                </div>
-                <div>
-                    <input id = "content" type="text" name="content" value={content} onChange={handleContent} placeholder="내용을 입력하세요." />
-                </div>
-                <button type="button" id="submitBtn" onClick={onClickSubmit}>신청</button>
-            </div>
+            </body>
         </div>
     )
 }
