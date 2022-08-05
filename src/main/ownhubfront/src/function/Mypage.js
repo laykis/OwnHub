b8466c3875b3020d4bc1c51fa14cc4e4480754b3
Goinging98 <yunw98@naver.com>
2022-08-06 00:46:22 +0700

    [webfront] mypage add

--- src/main/ownhubfront/src/function/Mypage.js
+++ src/main/ownhubfront/src/function/Mypage.js
@@ -1,14 +1,25 @@
-import React from "react";
-// import axios from "axios";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 import "../each_page.css";
 import Sidebar from "../layout/sidebar_member";
 
-const Mypage = () => {
+function Mypage() {
+    const [users, setUsers] = useState(null)
+    axios.get("url")
+
+    {/*
+    if(photos.length>0) {
+        return 
+    }*/}
+
     return (
         <div>
             <Sidebar/>
-            my page
-            <p id="name">김가영</p>
+            <div id="info">
+                <img id="face" src={require("../layout/img/face.png")}></img>
+                <p id="name">김가영</p>
+                <p id="team_role">OwnHub - IT개발본부 - 차세대 개발팀</p>
+            </div>
 
         </div>
     )
