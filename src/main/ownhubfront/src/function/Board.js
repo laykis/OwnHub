51e40555235b8708e17dece1245f2266c068489d
Goinging98 <yunw98@naver.com>
2022-08-08 00:42:06 +0700

    [webfront] update
    - 전반적인 레이아웃 수정
    - 휴가신청 부분 표로 재구성
    - react axios get과 post 부분 중점으로 작업 예정

--- src/main/ownhubfront/src/function/Board.js
+++ src/main/ownhubfront/src/function/Board.js
@@ -1,11 +1,12 @@
 import React, {useState} from "react";
 import axios from "axios";
-import "../each_page.css";
 import Sidebar from "../layout/sidebar_board";
+import "../each_page.css";
 
 function Board() {
     const [title, setTitle] = useState("")
     const [content, setContent] = useState("")
+    const [cboard, setSelected] = useState("")
 
     const handleTitle = (e) => {
         setTitle(e.target.value)
@@ -15,15 +16,21 @@ function Board() {
         setContent(e.target.value)
     }
 
+    const handleSelect = (e) => {
+        setSelected(e.target.value)
+    }
+
     const onClickSubmit = () =>{
         console.log("click submit")
         console.log("title =", title)
         console.log("content =", content)
+        console.log("type =", cboard)
 
         axios.post("/main/board/submit", null, {
             param: {
                 "title" : title,
-                "content" : content
+                "content" : content,
+                "cboard" : cboard
             }
         })
             .then(res => {
@@ -31,7 +38,7 @@ function Board() {
                 if(res.data.result === "sucsess"){
                     console.log("content post sucsess")
                     alert("게시 완료")
-                    window.location.href = "redirect:/main/board"
+                    window.location.href = "redirect:/content"
                 }
             })
             .catch()
@@ -41,23 +48,25 @@ function Board() {
 
     return (
         <div>
-            <Sidebar/>
-            <div id="writing">
-                <div>
-                    <select id="cboard">
-                        <option value="notice">공지사항</option>
-                        <option value="team">부서게시판</option>
-                        <option value="free">자유게시판</option>
-                    </select>
-                    <input id = "writing_title" type="text" name="title" value={title} onChange={handleTitle} placeholder="제목을 입력하세요." />
+            <body>
+                <Sidebar/>
+                <div id="writing">
+                    <div>
+                        <select id="cboard" value={cboard} onChange={handleSelect}>
+                            <option value="notice">공지사항</option>
+                            <option value="team">부서게시판</option>
+                            <option value="free">자유게시판</option>
+                        </select>
+                        <input id = "writing_title" type="text" name="title" value={title} onChange={handleTitle} placeholder="제목을 입력하세요." />
+                    </div>
+                    <div>
+                        <input id = "writing_content" type="text" name="content" value={content} onChange={handleContent} placeholder="내용을 입력하세요." />
+                    </div>
+                    <div>
+                    <button type="button" id="submitBtn" onClick={onClickSubmit}>글 올리기</button>
+                    </div>
                 </div>
-                <div>
-                    <input id = "writing_content" type="text" name="content" value={content} onChange={handleContent} placeholder="내용을 입력하세요." />
-                </div>
-                <div>
-                <button type="button" id="submitBtn" onClick={onClickSubmit}>글 올리기</button>
-                </div>
-            </div>
+            </body>
         </div>
     )
 
